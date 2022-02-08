39f2af6247fea0fd1bfadcf861c356b59a843a98
r0s <271257581@qq.com>
2022-02-08 19:57:01 +0800

    change https scheme to git scheme

--- push.sh
+++ push.sh
@@ -5,5 +5,5 @@ cd public
 git init
 git add .
 git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
-git remote add origin https://github.com/iamr0s/iamr0s.github.io.git
+git remote add origin git@github.com:iamr0s/iamr0s.github.io.git
 git push -u -f origin main
